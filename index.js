3e5c60248166396bd89233edeb618b11a9ad9053
Sara <saraeliza.lirio@gmail.com>
2022-07-14 16:32:28 -0300

    Testando express

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from 'express'
 
+
 const app = express()
 const port = 3001
 
 app.get('/t', (req,res) =>{
-    res.send('teste!!')
-})
+   return res.send('teste!!')
+});
+
+app.listen(port, ()=>{  
+    console.log(`Servidor online no endereço http://localhost:${port}`)
+});
