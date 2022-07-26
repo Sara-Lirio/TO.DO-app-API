0018d9093cd8ad1aa9845485651463d260000690
Sara <saraeliza.lirio@gmail.com>
2022-07-26 17:30:13 -0300

    Atualização rename

---
+++ src/middleware/autenticacao.js
@@ -0,0 +1,16 @@
+const autenticacao = (app)=>{
+    app.use((req, res, next)=>{
+        if(req.method !== "GET"){
+            if(req.headers.usertype === "gerente"){
+                next()
+            }else{
+                res.json({"erro" : "Usuario nao permitido"})
+            }
+        }else{
+            next()
+        }
+    })
+
+}
+
+export default autenticacao
